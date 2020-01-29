508feee3539e791c9e3150b8e1c7314bc173ca62
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-29 22:01:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,3 +39,13 @@ def map_to_square(source_array)
   end
   squared_arr
 end
+
+def reduce_to_total(source_array, starting_point=0)	
+  total = starting_point	
+  i = 0	
+  while i < source_array.length do	
+    total += source_array[i]	
+    i += 1	
+  end	
+  return total	
+end
