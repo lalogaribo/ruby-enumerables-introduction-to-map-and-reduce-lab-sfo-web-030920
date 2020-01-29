e51940e1f9c32fea9115ce40a3ef241e4ccdd7d1
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-29 21:24:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,10 @@
 # My Code here....
-
+def map_to_negativize(source_array)
+    negative_array = []
+    idx = 0
+  while idx < source_array.length
+    negative = source_array[idx] * -idx
+    negative_array << negative
+  end
+  negative_array
+end
